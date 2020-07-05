ddb15551b2d0951adc20b95f2481501c36dff895
rosharma08 <rohit.sharma@quovantis.com>
2020-07-05 22:40:04 +0530

    Updates

---
+++ precache-manifest.1591914e896bf88c7260cb9ed3f923e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbaa4e45c8d08d931cf0c4de0cadf32",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "e8e64ebbcf6db840a0a8",
+    "url": "/burger-builder/static/css/main.02d78125.chunk.css"
+  },
+  {
+    "revision": "f8c8baebd1be16f3067c",
+    "url": "/burger-builder/static/js/2.9ce1404b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.9ce1404b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e64ebbcf6db840a0a8",
+    "url": "/burger-builder/static/js/main.2cdb369d.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
